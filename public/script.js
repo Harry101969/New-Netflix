e43803da75d6445582cd361537f3e352d6ead976
Harsh Agarwal <harshagarwal19696@gmail.com>
2024-05-25 20:14:21 +0530

    New Netflix Updated

---
+++ public/script.js
@@ -0,0 +1,122 @@
+const apiEndpoint = "/api";
+const imgPath = "https://image.tmdb.org/t/p/original";
+
+const apiPaths = {
+  fetchAllCategories: `${apiEndpoint}/genres`,
+  fetchMoviesList: (id) => `${apiEndpoint}/movies?genreId=${id}`,
+  fetchTrending: `${apiEndpoint}/trending`,
+  searchOnYoutube: (query) => `${apiEndpoint}/search?query=${query}`
+};
+
+function init() {
+  fetchTrendingMovies();
+  fetchAndBuildAllSections();
+}
+
+function fetchTrendingMovies() {
+  fetchAndBuildMovieSection(apiPaths.fetchTrending, 'Trending Now').then(list => {
+    const randomIndex = parseInt(Math.random() * list.length);
+    buildBannerSection(list[randomIndex]);
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+function buildBannerSection(movie) {
+  const bannerCont = document.querySelector('#banner-section');
+  bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
+  bannerCont.style.backgroundPosition = 'contain';
+  const div = document.createElement('div');
+  div.innerHTML = `
+      <h2 class="movie_classy">Movie Information</h2>
+      <h2 class="banner__title">${movie.title}</h2>
+      <p class="banner__info">Trending in movies | Released:${movie.release_date}</p>
+      <p class="banner__overview">${movie.overview && movie.overview.length > 200 ? movie.overview.slice(0, 200).trim() + '...' : movie.overview}</p>
+      <div class="action-buttons-cont">
+        <button class="action-button">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ltr-4z3qvp e1svuwfo1" data-name="Play" aria-hidden="true">
+            <path d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z" fill="currentColor"></path>
+          </svg> &nbsp;&nbsp; Play 
+        </button>
+        <button class="action-button">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ltr-4z3qvp e1svuwfo1" data-name="CircleI" aria-hidden="true">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path>
+          </svg>  &nbsp;&nbsp; More Info
+        </button>
+      </div>`;
+  div.className = "banner-content container";
+  bannerCont.append(div);
+}
+
+function fetchAndBuildAllSections() {
+  fetch(apiPaths.fetchAllCategories)
+    .then(res => res.json())
+    .then(res => {
+      const categories = res.genres;
+      if (Array.isArray(categories) && categories.length) {
+        categories.forEach(category => {
+          fetchAndBuildMovieSection(apiPaths.fetchMoviesList(category.id), category.name);
+        });
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+function fetchAndBuildMovieSection(fetchUrl, categoryName) {
+  console.log(fetchUrl, categoryName);
+  return fetch(fetchUrl)
+    .then(res => res.json())
+    .then(res => {
+      console.table(res.results);
+      const movies = res.results;
+      if (Array.isArray(movies) && movies.length) {
+        buildMoviesSection(movies, categoryName);
+      }
+      return movies;
+    })
+    .catch(err => console.log(err));
+}
+
+function buildMoviesSection(list, categoryName) {
+  console.log(list, categoryName);
+  const moviesCont = document.querySelector('#movies-cont');
+  const moviesListHTML = list.map(item => {
+    return `<div class="movie-item" ondblclick="searchMovieTrailer('${item.title}', 'yt${item.id}')">
+              <img class="movie-item-img" src="${imgPath}${item.backdrop_path}" alt="${item.title}">
+              <iframe width="245px" height="150px" src="" id="yt${item.id}"></iframe>
+            </div>`;
+  }).join('');
+  const moviesSectionHTML = `
+      <h2 class="movie-section-heading">${categoryName}<span class="explore-nudge">Explore All</span></h2>
+      <div class="movies-row">
+        ${moviesListHTML}
+      </div>`;
+  console.log(moviesSectionHTML);
+  const div = document.createElement('div');
+  div.className = "movies-section";
+  div.innerHTML = moviesSectionHTML;
+  moviesCont.append(div);
+}
+
+function searchMovieTrailer(movieName, iframId) {
+  if (!movieName) return;
+  fetch(apiPaths.searchOnYoutube(movieName))
+    .then(res => res.json())
+    .then(res => {
+      const bestResult = res.items[0];
+      const youtubeUrl = `https://www.youtube.com/watch?v=${bestResult.id.videoId}`;
+      console.log(youtubeUrl);
+      window.open(youtubeUrl, '_blank');
+      const elements = document.getElementById(iframId);
+      elements.src = `https://www.youtube.com/embed/${bestResult.id.videoId}?=1&controls=0`;
+    }).catch(err => console.log(err));
+}
+
+window.addEventListener('load', function () {
+  init();
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 5) header.classList.add('black-bg');
+    else header.classList.remove('black-bg');
+  });
+});
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const apiEndpoint = "https://api.themoviedb.org/3";
+const apiKey = process.env.API_KEY;
+
+app.use(express.static('public'));
+
+app.get('/api/genres', async (req, res) => {
+    try {
+        const response = await axios.get(`${apiEndpoint}/genre/movie/list?api_key=${apiKey}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/api/movies', async (req, res) => {
+    const { genreId } = req.query;
+    try {
+        const response = await axios.get(`${apiEndpoint}/discover/movie?api_key=${apiKey}&with_genres=${genreId}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/api/trending', async (req, res) => {
+    try {
+        const response = await axios.get(`${apiEndpoint}/trending/all/day?api_key=${apiKey}&language=en-US`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/api/search', async (req, res) => {
+    const { query } = req.query;
+    try {
+        const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyALflbsAUPWoNvgyEJ35u2I-1aP_rUwX84`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(3000, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
